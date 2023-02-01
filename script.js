90c8e3f334ef755fe8dab43915074f1cd45ab3d5
maggyirungu <101802855+maggyirungu@users.noreply.github.com>
2023-02-01 12:23:40 +0300

    sign up section

---
+++ script.js
@@ -0,0 +1,4 @@
+let signupBtn = document.getElementById("signupBtn");
+let signinBtn = document.getElementById("signinBtn");
+let nameField = document.getElementById("nameField");
+let title = document.getElementById("title");
